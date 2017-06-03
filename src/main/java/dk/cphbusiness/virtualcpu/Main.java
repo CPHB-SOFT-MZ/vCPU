64064d41fc5fa58c5798d22007c130be0303048b
Mikkel Ziemer <mzhjensen@gmail.com>
2017-06-03 14:35:09 +0200

    Added instruction translation from strings to binary

--- src/main/java/dk/cphbusiness/virtualcpu/Main.java
+++ src/main/java/dk/cphbusiness/virtualcpu/Main.java
@@ -17,20 +17,32 @@ public class Main {
                 "00010000", "00001100", "11000110", "00010011", "00010010", 
                 "00000010", "00100001", "00011000");
         
+        
         //Tail recursive factorial of 5
         Program tailFact = new Program("01000010", "00010000", "01001010", "00010000", 
                 "00001100", "11001000", "00010010", "00001111", 
                 "00110010", "00000111", "10001100", "00011001", 
                 "00110101", "00000010", "00100010", "00110010", "00010111", "00100001", "00001100", "10001000");
+        
+        Program tailFactInstr = new Program("MOV 1 A", "PUSH A", "MOV 5 A", "PUSH A", "ALWAYS", 
+                "CALL #8", "POP A", "HALT", "MOV +1 A", "NZERO", "JMP #12", "RTN +1", "MOV +2 B", "MUL",
+                "MOV A +2", "MOV +1 A", "DEC", "MOV A +1", "ALWAYS", "JMP #8");
+        
+        Program normFactInstr = new Program("MOV 5 A", "PUSH A", "ALWAYS", "CALL #6", "POP A", "HALT", "MOV +1 A", 
+                "NZERO", "JMP #12", "MOV 1 A", "MOV A +1", "RTN +0", "PUSH A", "DEC", "PUSH A", "ALWAYS", "CALL #6", 
+                "POP B", "POP A", "MUL", "MOV A +1", "RTN +0");
+        
         Machine machine = new Machine();
         
         
-        machine.load(tailFact);
+        machine.load(normFactInstr);
         machine.print(System.out);
+        
         while(true){
             machine.tick();
             machine.print(System.out);
         }
+        
     }
 
 }
--- src/main/java/dk/cphbusiness/virtualcpu/Program.java
+++ src/main/java/dk/cphbusiness/virtualcpu/Program.java
@@ -16,11 +16,85 @@ public class Program implements Iterable<Integer> {
             return Integer.parseInt(line, 2);
         } else if (line.startsWith("MOV ")) {
             String[] parts = line.split(" ");
-            int r = parts[1].equals("B") ? 1 : 0;
-            int o = Integer.parseInt(parts[2]);
             
-            return 0b0010_0000 | (r << 3) | o;
-        } else {
+            if(parts[1].contains("+")){
+                int r = parts[2].equals("B") ? 1 : 0;
+                int o = Integer.parseInt(parts[1].replace("+", ""));
+                return 0b0011_0000 | (o << 1) | r;
+            } else if (parts[2].contains("+")) {
+                int r = parts[1].equals("B") ? 1 : 0;
+                int o = Integer.parseInt(parts[2].replace("+", ""));
+                return 0b0010_0000 | (r << 3) | o;
+            } else {
+                int r = parts[2].equals("B") ? 1 : 0;
+                int v = Integer.parseInt(parts[1]);
+
+                return 0b0100_0000 | (v << 1) | r;
+            }
+            
+        } else if (line.equals("ADD")) {
+            return 0b0000_0001;
+        } else if (line.equals("NOP")){
+            return 0b0000_0000;
+        } else if (line.equals("MUL")){
+            return 0b0000_0010;
+        } else if (line.equals("DIV")){
+            return 0b0000_0011;
+        } else if (line.equals("ZERO")){
+            return 0b0000_0100;
+        } else if (line.equals("NEG")){
+            return 0b0000_0101;
+        } else if (line.equals("POS")){
+            return 0b0000_0110;
+        } else if (line.equals("NZERO")){
+            return 0b0000_0111;
+        } else if (line.equals("EQ")){
+            return 0b0000_1000;
+        } else if (line.equals("LT")){
+            return 0b0000_1001;
+        } else if (line.equals("GT")){
+            return 0b0000_1010;
+        } else if (line.equals("NEQ")){
+            return 0b0000_1011;
+        } else if (line.equals("ALWAYS")){
+            return 0b0000_1100;
+        } else if (line.equals("HALT")){
+            return 0b0000_1111;
+        } else if (line.startsWith("PUSH ")){
+            String[] parts = line.split(" ");
+            int r = parts[1].equals("B") ? 1 : 0;
+            
+            return 0b0001_0000 | r;
+            
+        } else if (line.startsWith("POP ")){
+            String[] parts = line.split(" ");
+            int r = parts[1].equals("B") ? 1 : 0;
+            
+            return 0b0001_0010 | r;
+        } else if (line.equals("INC")){
+            return 0b0001_0110;
+        } else if (line.equals("DEC")){
+            return 0b0001_0111;
+        } else if(line.equals("MOV A B")){
+            return 0b0001_0100;
+        } else if (line.equals("MOV B A")){
+            return 0b0001_0101;
+        } else if (line.startsWith("RTN ")){
+            String[] parts = line.split(" ");
+            int o = Integer.parseInt(parts[1].replace("+", ""));
+            return 0b0001_1000 | o;
+        } else if (line.equals("RTN")){
+            return 0b0001_1000;
+        } else if (line.startsWith("JMP ")){
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[1].replace("#", ""));
+            return 0b1000_0000 | a;
+        } else if (line.startsWith("CALL ")){
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[1].replace("#", ""));
+            return 0b1100_0000 | a;
+        }
+        else {
             throw new UnsupportedOperationException("Don't know " + line);
         }
     }
